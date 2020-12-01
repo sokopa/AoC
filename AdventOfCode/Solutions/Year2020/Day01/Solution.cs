3658c51081a6c44a0c108346ecd6fd365099bdd8
Sotiris Koukios-Panopoulos <sokopa@gmail.com>
2020-12-01 09:07:52 +0200

    2020 - Day 1 - Part 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -7,15 +7,31 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day01 : ASolution
     {
-
+        private List<int> _inputs;
         public Day01() : base(01, 2020, "")
         {
-
+            var lines = Input.SplitByNewline();
+            _inputs = new List<int>();
+            foreach (var line in lines)
+            {
+                _inputs.Add(int.Parse(line));
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var differences = new HashSet<int>();
+            foreach (var expense in _inputs)
+            {
+                if (differences.Contains(2020 - expense))
+                {
+                    return (expense * (2020 - expense)).ToString();
+                }
+
+                differences.Add(expense);
+            }
+
+            return "None found";
         }
 
         protected override string SolvePartTwo()
