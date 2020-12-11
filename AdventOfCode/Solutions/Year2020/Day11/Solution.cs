6249f89e56074cc8e15bd6aa9b0a2dca910e2d82
sokopa <sokopa@gmail.com>
2020-12-11 08:20:15 +0200

    2020 - Day 11 - Part 2

--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -8,6 +8,7 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day11 : ASolution
     {
+        // this reminds me a lot of Conway's game of life
         private GameOfSeatsSimulation sim;
         public Day11() : base(11, 2020, "")
         {
@@ -28,14 +29,19 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var changed = false;
+            do
+            {
+                changed = sim.Update(sim.CountFirstVisibleOccupiedAround, 5);
+            } while (changed);
+
+            return sim.NumberOfOccupiedSeats().ToString();
         }
     }
 
     public class GameOfSeatsSimulation
     {
         private State[,] floorplan;
-        private Stack<State[,]> history;
         private int width;
         private int height;
         public void Init(string[] input)
@@ -43,7 +49,6 @@ namespace AdventOfCode.Solutions.Year2020
             width = input[0].Length;
             height = input.Length;
             floorplan = new State[width,height];
-            history = new Stack<State[,]>();
             for (int r = 0; r < input.Length; r++)
             {
                 var row = input[r];
@@ -52,27 +57,28 @@ namespace AdventOfCode.Solutions.Year2020
                     floorplan[c, r] = (State) row[c];
                 }
             }
-            //DrawBoard();
-            Console.WriteLine($"Occupied seats: {NumberOfOccupiedSeats()}");
-            history.Push(floorplan);
         }
 
-        public bool Update()
+        public bool Update(Func<int, int, int> countOccupied = null, int threshold = 4)
         {
+            if (countOccupied == null)
+            {
+                countOccupied = CountOccupiedAround;
+            }
             var changed = false;
             var newPlan = new State[width, height];
             for (int row = 0; row < height; row++)
             for (int col = 0; col < width; col++)
             {
                 var current = floorplan[col, row];
-                var numOccupied = CountOccupiedAround(row, col);
+                var numOccupied = countOccupied(row, col);
                 switch (current)
                 {
                     case State.Empty when numOccupied == 0:
                         newPlan[col, row] = State.Occupied;
                         changed = true;
                         break;
-                    case State.Occupied when numOccupied >= 4:
+                    case State.Occupied when numOccupied >= threshold:
                         newPlan[col, row] = State.Empty;
                         changed = true;
                         break;
@@ -83,13 +89,12 @@ namespace AdventOfCode.Solutions.Year2020
             }
 
             floorplan = newPlan;
-            history.Push(floorplan);
             //DrawBoard();
-            Console.WriteLine($"Occupied seats: {NumberOfOccupiedSeats()}");
+            //Console.WriteLine($"Occupied seats: {NumberOfOccupiedSeats()}");
             return changed;
         }
 
-        private int CountOccupiedAround(int col, int row)
+        public int CountOccupiedAround(int col, int row)
         {
             return IsNeighborOccupied(row, col, -1, -1)
                    + IsNeighborOccupied(row, col, -1, 0)
@@ -100,7 +105,7 @@ namespace AdventOfCode.Solutions.Year2020
                    + IsNeighborOccupied(row, col, +1, 0)
                    + IsNeighborOccupied(row, col, +1, +1);
         }
-
+        
         private int IsNeighborOccupied(int x, int y, int xOffset, int yOffset)
         {
             var result = 0;
@@ -110,6 +115,39 @@ namespace AdventOfCode.Solutions.Year2020
             if (!outOfBounds)
             {
                 result = floorplan[newX, newY] == State.Occupied ? 1 : 0;
+            }
+            return result;
+        }
+
+        public int CountFirstVisibleOccupiedAround(int col, int row)
+        {
+            return IsVisibleNeighborOccupied(row, col, -1, -1)
+                   + IsVisibleNeighborOccupied(row, col, -1, 0)
+                   + IsVisibleNeighborOccupied(row, col, -1, +1)
+                   + IsVisibleNeighborOccupied(row, col, 0, -1)
+                   + IsVisibleNeighborOccupied(row, col, 0, +1)
+                   + IsVisibleNeighborOccupied(row, col, +1, -1)
+                   + IsVisibleNeighborOccupied(row, col, +1, 0)
+                   + IsVisibleNeighborOccupied(row, col, +1, +1);
+        }
+
+        private int IsVisibleNeighborOccupied(int x, int y, int xOffset, int yOffset)
+        {
+            var result = 0;
+            int newX = x + xOffset;
+            int newY = y + yOffset;
+            var outOfBounds = newX < 0 || newX >= width || newY < 0 || newY >= height;
+            if (!outOfBounds)
+            {
+                if (floorplan[newX, newY] == State.Floor)
+                {
+                    // if i take the sign, it will only add +1,0,-1 to the direction x or y is already looking to
+                    result = IsVisibleNeighborOccupied(x, y, xOffset + Math.Sign(xOffset), yOffset + Math.Sign(yOffset));
+                }
+                else
+                {
+                    result = floorplan[newX, newY] == State.Occupied ? 1 : 0;
+                }
             }
             return result;
         }
