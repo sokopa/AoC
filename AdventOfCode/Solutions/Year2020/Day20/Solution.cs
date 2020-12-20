90401bdbd132c0b592267a3bd97ca1905a8a4dbe
sokopa <sokopa@gmail.com>
2020-12-20 09:17:04 +0200

    2020 - Day 20 - Part 1

--- AdventOfCode/Solutions/Year2020/Day20/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day20/Solution.cs
@@ -1,23 +1,179 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day20 : ASolution
+    internal class Day20 : ASolution
     {
+        private Dictionary<int, Tile> _tiles;
 
         public Day20() : base(20, 2020, "")
         {
+//             DebugInput = @"Tile 2311:
+// ..##.#..#.
+// ##..#.....
+// #...##..#.
+// ####.#...#
+// ##.##.###.
+// ##...#.###
+// .#.#.#..##
+// ..#....#..
+// ###...#.#.
+// ..###..###
+//
+// Tile 1951:
+// #.##...##.
+// #.####...#
+// .....#..##
+// #...######
+// .##.#....#
+// .###.#####
+// ###.##.##.
+// .###....#.
+// ..#.#..#.#
+// #...##.#..
+//
+// Tile 1171:
+// ####...##.
+// #..##.#..#
+// ##.#..#.#.
+// .###.####.
+// ..###.####
+// .##....##.
+// .#...####.
+// #.##.####.
+// ####..#...
+// .....##...
+//
+// Tile 1427:
+// ###.##.#..
+// .#..#.##..
+// .#.##.#..#
+// #.#.#.##.#
+// ....#...##
+// ...##..##.
+// ...#.#####
+// .#.####.#.
+// ..#..###.#
+// ..##.#..#.
+//
+// Tile 1489:
+// ##.#.#....
+// ..##...#..
+// .##..##...
+// ..#...#...
+// #####...#.
+// #..#.#.#.#
+// ...#.#.#..
+// ##.#...##.
+// ..##.##.##
+// ###.##.#..
+//
+// Tile 2473:
+// #....####.
+// #..#.##...
+// #.##..#...
+// ######.#.#
+// .#...#.#.#
+// .#########
+// .###.#..#.
+// ########.#
+// ##...##.#.
+// ..###.#.#.
+//
+// Tile 2971:
+// ..#.#....#
+// #...###...
+// #.#.###...
+// ##.##..#..
+// .#####..##
+// .#..####.#
+// #..#.#..#.
+// ..####.###
+// ..#.#.###.
+// ...#.#.#.#
+//
+// Tile 2729:
+// ...#.#.#.#
+// ####.#....
+// ..#.#.....
+// ....#..#.#
+// .##..##.#.
+// .#.####...
+// ####.#.#..
+// ##.####...
+// ##..#.##..
+// #.##...##.
+//
+// Tile 3079:
+// #.#.#####.
+// .#..######
+// ..#.......
+// ######....
+// ####.#..#.
+// .#...#.##.
+// #.#####.##
+// ..#.###...
+// ..#.......
+// ..#.###...";
 
+            _tiles = new Dictionary<int, Tile>();
+            var tiles = Input.Split("\n\n");
+            foreach (var tile in tiles)
+            {
+                var parsedTile = new Tile(tile.SplitByNewline());
+                _tiles.Add(parsedTile.Id, parsedTile);
+            }
         }
+
 
         protected override string SolvePartOne()
         {
-            return null;
-        }
+            // corner puzzle pieces only have 2 edges matched, the other two unmatched.
+            // Pass 1: populate the edge map
+            var edgeCounts = new Dictionary<string, int>();
 
+            foreach (var tile in _tiles.Values)
+            {
+                var edges = tile.GetEdges();
+                AddEdgeToDict(edgeCounts, edges.North);
+                AddEdgeToDict(edgeCounts, edges.East);
+                AddEdgeToDict(edgeCounts, edges.South);
+                AddEdgeToDict(edgeCounts, edges.West);
+            }
+
+            var cornerProduct = 1L;
+            foreach (var tile in _tiles)
+            {
+                var edges = tile.Value.GetEdges();
+                var count = GetEdgeCountFromDict(edgeCounts, edges.North) 
+                            + GetEdgeCountFromDict(edgeCounts, edges.East) 
+                            + GetEdgeCountFromDict(edgeCounts, edges.South) 
+                            + GetEdgeCountFromDict(edgeCounts, edges.West);
+                if (count == 6) // 4 occurences of its own edges, + 2 for the matching ones
+                    cornerProduct *= tile.Key;
+            }
+
+            return cornerProduct.ToString();
+            
+            void AddEdgeToDict(Dictionary<string, int> edgeCounts, string edge)
+            {
+                var rev = edge.Reverse();
+                if (edgeCounts.ContainsKey(edge))
+                    edgeCounts[edge]++;
+                else if (edgeCounts.ContainsKey(rev))
+                    edgeCounts[rev]++;
+                else edgeCounts[edge] = 1;
+            }
+            int GetEdgeCountFromDict(Dictionary<string, int> edgeCounts, string edge)
+            {
+                if (!edgeCounts.TryGetValue(edge, out var val)) edgeCounts.TryGetValue(edge.Reverse(), out val);
+                return val;
+            }
+        }
+        
         protected override string SolvePartTwo()
         {
             return null;
---
+++ AdventOfCode/Solutions/Year2020/Day20/Tile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    public class Tile
+    {
+        public Tile(string[] data)
+        {
+            var tile = data.First().Split(" ")[1];
+            var tileId = tile.Substring(0, tile.Length - 1);
+
+            Id = int.Parse(tileId);
+            Content = data.Skip(1).ToArray();
+            Rotation = 0;
+            FlipDirection = FlipDirection.None;
+        }
+        
+        public int Id { get; private set; }
+        public string[] Content { get; private set; }
+
+        public int Width => Content[0].Length;
+        public int Height => Content.Length;
+
+        public int Rotation { get; set; } // 0: as is. 1: 90 deg CCW, 2: 180 deg CCW, 3: 270 deg CCW.
+        public FlipDirection FlipDirection {get; set;}
+
+        public Dictionary<Direction, Tile> Neighbors = new Dictionary<Direction, Tile>();
+
+        public (string North, string East, string South, string West) GetEdges() => GetEdgesWithFlipAndRotation();
+        
+        private (string North, string East, string South, string West) GetEdgesWithFlipAndRotation()
+        {
+            var edges = GetEdgesWithRotation();
+            return (this.FlipDirection) switch
+            {
+                FlipDirection.None => (edges.North, edges.East, edges.South, edges.West),
+                FlipDirection.Horizontal => // the axis is horizontal. west/east are reversed, north/south are switched
+                    (edges.South, edges.East.Reverse(), edges.North, edges.West.Reverse()),
+                FlipDirection.Vertical => // the axis is vertical. west/east are switched, n/s reversed
+                    (edges.North.Reverse(), edges.West, edges.South.Reverse(), edges.East),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        
+        private (string North, string East, string South, string West) GetEdgesWithRotation()
+        {
+            var edges = GetEdgesWithoutRotation();
+
+            return (Rotation % 4) switch
+            {
+                0 => (edges.North, edges.East, edges.South, edges.West),
+                1 => (edges.East, edges.South.Reverse(), edges.West, edges.North.Reverse()),
+                2 => (edges.South.Reverse(), edges.West.Reverse(), edges.North.Reverse(), edges.East.Reverse()),
+                3 => (edges.West.Reverse(), edges.North, edges.East.Reverse(), edges.South),
+                _ => throw new ArgumentException(nameof(Rotation))
+            };
+        }
+        
+        private (string North, string East, string South, string West) GetEdgesWithoutRotation()
+        {
+            var north = Content[0];
+            var south = Content[^1];
+            var west = string.Concat(Content.Select(s => s[0]));
+            var east = string.Concat(Content.Select(s => s[^1]));
+            return (north, east, south, west);
+        }
+        
+    }
+
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public enum FlipDirection
+    {
+        None,
+        Horizontal,
+        Vertical
+    }
+}
