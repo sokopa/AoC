c80c88aaea064b207892ed0670c191c8e77c8881
sokopa <sokopa@gmail.com>
2020-12-23 09:29:23 +0200

    2020 - Day 23 - Both parts

---
+++ AdventOfCode/Solutions/Year2020/Day23/CircularList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    internal class CircularList
+
+    {
+        private int data;
+        public int Data => data;
+
+        private CircularList next;
+        public CircularList Next => next;
+
+
+        public CircularList()
+        {
+            data = 0;
+
+            next = this;
+        }
+
+
+        public CircularList(int value)
+
+        {
+            data = value;
+
+            next = this;
+        }
+
+
+        public CircularList InsertNext(int value)
+
+        {
+            var node = new CircularList(value);
+
+            if (next == this) // only one node in the circular list
+
+            {
+                // Easy to handle, after the two lines of executions,
+
+                // there will be two nodes in the circular list
+
+                node.next = this;
+
+                next = node;
+            }
+
+            else
+
+            {
+                // Insert in the middle
+
+                (node.next, next) = (next, node);
+            }
+
+            return node;
+        }
+
+        public CircularList InsertNext(CircularList node)
+
+        {
+            if (next == this) // only one node in the circular list
+
+            {
+                // Easy to handle, after the two lines of executions,
+
+                // there will be two nodes in the circular list
+
+                node.next = this;
+
+                next = node;
+            }
+
+            else
+
+            {
+                // Insert in the middle
+
+                (node.next, next) = (next, node);
+            }
+
+            return node;
+        }
+
+
+        public CircularList DeleteNext()
+
+        {
+            if (next == this)
+
+            {
+                Console.WriteLine(
+                    "\nThe node can not be deleted as there is only one node in the circular list");
+
+                return null;
+            }
+
+
+            var node = next;
+
+            next = next.next;
+
+            return node;
+        }
+
+        public void Traverse(CircularList node, Action<CircularList> action)
+
+        {
+            if (node == null)
+                node = this;
+
+            var startnode = node;
+            do
+            {
+                action(node);
+                node = node.next;
+            } while (node != startnode);
+        }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day23/Solution.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +9,113 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day23 : ASolution
     {
-
+        private List<int> _cups;
         public Day23() : base(23, 2020, "")
         {
-
+            _cups = new List<int>
+            {
+                5, 8, 3, 9, 7, 6, 2, 4, 1
+            };
+            
+            // _cups = new List<int>
+            // {
+            //     3,8,9,1,2,5,4,6,7
+            // };
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var max_cup = 9;
+            var ind = new Dictionary<int, CircularList>();
+
+            var head = new CircularList(_cups[0]);
+            ind[_cups[0]] = head;
+            var curr = head;
+            
+            foreach (var cup in _cups)
+            {
+                if (cup == _cups[0]) continue;
+                curr = curr.InsertNext(cup);
+                ind[cup] = curr;
+            }
+
+            var currCupNum = head.Data;
+            for (int i = 0; i < 100; i++)
+            {
+                var currentCup = ind[currCupNum];
+                var c1 = currentCup.DeleteNext();
+                var c2 = currentCup.DeleteNext();
+                var c3 = currentCup.DeleteNext();
+
+                var target = Subtract(currCupNum, new[] {c1.Data, c2.Data, c3.Data});
+                var targetNode = ind[target];
+                targetNode.InsertNext(c3);
+                targetNode.InsertNext(c2);
+                targetNode.InsertNext(c1);
+                currCupNum = currentCup.Next.Data;
+            }
+
+            var start = ind[1].Next;
+            var sb = new StringBuilder();
+            start.Traverse(start, node => sb.Append(node.Data));
+            return sb.ToString().Replace("1", "");
+
+
         }
 
+        private int Subtract(int curr,  int[] notIn, int maxCup = 9)
+        {
+            var target = (curr - 1) % maxCup;
+            if (target <= 0) target = maxCup;
+            while (notIn.Any(c => c == target))
+            {
+                target = (target - 1) % maxCup;
+                if (target <= 0) target = maxCup;
+            }
+            return target;
+        }
+       
         protected override string SolvePartTwo()
         {
-            return null;
+            var max_cup = 1000000;
+            var ind = new Dictionary<int, CircularList>();
+
+            var head = new CircularList(_cups[0]);
+            ind[_cups[0]] = head;
+            var curr = head;
+            
+            foreach (var cup in _cups)
+            {
+                if (cup == _cups[0]) continue;
+                curr = curr.InsertNext(cup);
+                ind[cup] = curr;
+            }
+
+            for (int i = 10; i <= max_cup; i++)
+            {
+                curr = curr.InsertNext(i);
+                ind[i] = curr;
+            }
+            var currCup = head;
+            for (int i = 0; i < 10000000; i++)
+            {
+                var c1 = currCup.DeleteNext();
+                var c2 = currCup.DeleteNext();
+                var c3 = currCup.DeleteNext();
+
+                var target = Subtract(currCup.Data, new[] {c1.Data, c2.Data, c3.Data}, max_cup);
+                var targetNode = ind[target];
+                targetNode.InsertNext(c3);
+                targetNode.InsertNext(c2);
+                targetNode.InsertNext(c1);
+                currCup = currCup.Next;
+            }
+
+            var firstStar = ind[1].Next;
+            var secondStar = ind[1].Next.Next;
+            long mul = (long)firstStar.Data * (long)secondStar.Data;
+
+            return mul.ToString();
         }
     }
 }
